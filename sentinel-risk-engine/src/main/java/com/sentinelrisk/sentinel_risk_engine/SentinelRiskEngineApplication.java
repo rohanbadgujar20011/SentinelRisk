6cbdab6e9bb4be8d562dc99b4bc3707a077cba20
rohanbadgujar20011 <rohanbadgujar2001@gmail.com>
2026-06-29 21:08:55 +0530

    Create Service for Rule Engine

---
+++ sentinel-risk-engine/src/main/java/com/sentinelrisk/sentinel_risk_engine/SentinelRiskEngineApplication.java
@@ -0,0 +1,13 @@
+package com.sentinelrisk.sentinel_risk_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelRiskEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelRiskEngineApplication.class, args);
+	}
+
+}
---
+++ sentinel-risk-engine/src/test/java/com/sentinelrisk/sentinel_risk_engine/SentinelRiskEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinelrisk.sentinel_risk_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelRiskEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
