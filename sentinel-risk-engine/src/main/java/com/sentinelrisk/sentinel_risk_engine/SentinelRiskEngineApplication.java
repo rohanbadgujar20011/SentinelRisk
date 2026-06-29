100fa1520e457fad215f61ab7d14574104e8ff29
rohanbadgujar20011 <rohanbadgujar2001@gmail.com>
2026-06-29 22:21:33 +0530

    Added Rule engine , Fix Pom.xml , Add docker compose

--- sentinel-risk-engine/src/main/java/com/sentinelrisk/sentinel_risk_engine/SentinelRiskEngineApplication.java
+++ sentinel-risk-engine/src/main/java/com/sentinelrisk/sentinel_risk_engine/SentinelRiskEngineApplication.java
@@ -2,11 +2,14 @@ package com.sentinelrisk.sentinel_risk_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.time.ZoneId;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class SentinelRiskEngineApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SentinelRiskEngineApplication.class, args);
 	}
 
